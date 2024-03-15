f4bb7129914aba3860c2393fdb4aecf211c5bec7
황승아 <mina789@naver.com>
2024-03-15 23:42:16 +0900

    엔티티 생성 및 테스트 작성

---
+++ sbb/src/main/java/com/mysite/demo/Question.java
@@ -0,0 +1,39 @@
+package com.mysite.demo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity // JPA 가 엔티티로 인식
+public class Question {
+	@Id // 기본 키로 지정
+	/*  
+	 * @GeneratedValue : 데이터를 저장할 때 해당 속성에 값을 따로 세팅하지 않아도 1 씩 자동으로 증가하여 저장
+	 * strategy: 고유번호를 생성하는 옵션
+	 * GenerationType.IDENTITY: 해당 컬럼만의 독립적인 시퀀스를 생성하여 번호를 증가시킬 때 사용
+	 * */
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(length=200) //  테이블의 컬럼명과 일치, length 는 컬럼의 길이
+	private String subject;
+	
+	@Column(columnDefinition = "TEXT") // columnDefinition : 컬럼의 속성을 정의할 때 사용
+	private String content;
+	
+	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	private List<Answer> answerList;
+	
+}
--- sbb/src/test/java/com/mysite/demo/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/demo/SbbApplicationTests.java
@@ -1,13 +1,30 @@
 package com.mysite.demo;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest                           
 class SbbApplicationTests {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무 엇 인 가 요?");
+		q1.setContent("sbb에 대 해 서 알 고 싶 습 니 다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1); // 첫 번 째 질 문 저 장
+		
+		Question q2 = new Question();
+		q2.setSubject("스 프 링 부 트 모 델 질 문 입 니 다.");
+		q2.setContent("id는 자 동 으 로 생 성 되 나 요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2); // 두 번 째 질 문 저 장
 	}
 
 }
