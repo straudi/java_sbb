cf3a385803b8d7aba14fcacca676593e446f26b0
황승아 <mina789@naver.com>
2024-03-18 18:07:41 +0900

    데이터 조회

--- sbb/src/main/java/com/mysite/demo/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/demo/QuestionRepository.java
@@ -1,7 +1,14 @@
 package com.mysite.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
-	
+	/* 메서드 선언하고 구현X 실행되는 이유 : JpaRep ository 를 상속한 QuestionRepository 객체가 생성될 때 (DI 에 의해 스프링이 자동으로 QuestionRepository 객체를 생성한다. 이 때 프록시 패턴이 사용된다고 한다.) 
+	 * 리포지터리 객체의 메서드가 실행될 때 JPA가 해당 메서드명을 분석하여 쿼리를 만들고 실행, findBy + 엔티티의 속성명(예:findBySubject) 과 같은 리포지터리 메서드를 작성하면 해당 속성의 값으로 데이터를 조회 가능
+	 * */
+	Question findBySubject(String subject); // 제목 기준으로 검색 추가할 경우 메서드를 인터페이스에 선언
+	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBySubjectLike(String subject);
 }
--- sbb/src/test/java/com/mysite/demo/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/demo/SbbApplicationTests.java
@@ -1,6 +1,10 @@
 package com.mysite.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +18,47 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무 엇 인 가 요?");
-		q1.setContent("sbb에 대 해 서 알 고 싶 습 니 다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1); // 첫 번 째 질 문 저 장
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무 엇 인 가 요?");
+//		q1.setContent("sbb에 대 해 서 알 고 싶 습 니 다.");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1); // 첫 번 째 질 문 저 장
+//		
+//		Question q2 = new Question();
+//		q2.setSubject("스 프 링 부 트 모 델 질 문 입 니 다.");
+//		q2.setContent("id는 자 동 으 로 생 성 되 나 요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2); // 두 번 째 질 문 저 장
 		
-		Question q2 = new Question();
-		q2.setSubject("스 프 링 부 트 모 델 질 문 입 니 다.");
-		q2.setContent("id는 자 동 으 로 생 성 되 나 요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2); // 두 번 째 질 문 저 장
+		// findAll
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(2, all.size());
+//		
+//		Question q = all.get(0);
+//		assertEquals("sbb가 무 엇 인 가 요?", q.getSubject());
+		
+		// findById
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		if(oq.isPresent()) {
+//			Question q = oq.get();
+//			assertEquals("sbb가 무 엇 인 가 요?", q.getSubject());
+//		}
+		
+		// findBySubject
+//		Question q = this.questionRepository.findBySubject("sbb가 무 엇 인 가 요?");
+//		assertEquals(1, q.getId());
+		
+		// findBySubjectAndContent
+//		Question q = this.questionRepository.findBySubjectAndContent(
+//				"sbb가 무 엇 인 가 요?", "sbb에 대 해 서 알 고 싶 습 니 다.");
+//		assertEquals(1, q.getId());
+		
+		// findBySubjectLike
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무 엇 인 가 요?", q.getSubject());
+		
+		
 	}
 
 }
