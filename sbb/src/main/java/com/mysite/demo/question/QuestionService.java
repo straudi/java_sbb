664eca24ff46fdfd2e486c3f8b9a4c8ee1b2b7a7
황승아 <mina789@naver.com>
2024-03-24 21:10:53 +0900

    서비스 추가

---
+++ sbb/src/main/java/com/mysite/demo/question/QuestionService.java
@@ -0,0 +1,17 @@
+package com.mysite.demo.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+	private final QuestionRepository questionRepository;
+	
+	public List<Question> getList() {
+		return this.questionRepository.findAll();
+	}
+}
