8a5f1d13d1eb50b99434d340ee45ffb363156f5b
황승아 <mina789@naver.com>
2024-03-27 01:02:51 +0900

    URL 프리픽스 (prefix)

--- sbb/src/main/java/com/mysite/demo/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/demo/question/QuestionController.java
@@ -6,17 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	//private final QuestionRepository questionRepository;
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	//@ResponseBody
 	public String list(Model model) {
 		//List<Question> questionList = this.questionRepository.findAll();
@@ -25,7 +27,7 @@ public class QuestionController {
 		return "question_list";
 	}
 	
-	@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
--- sbb/src/main/java/com/mysite/demo/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/demo/question/QuestionService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mysite.demo.DataNotFoundException;
 
