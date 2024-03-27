131d362f87315da7b18fb87b31873f4ae09da78a
황승아 <mina789@naver.com>
2024-03-27 22:48:38 +0900

    답변 등록하기

---
+++ sbb/src/main/java/com/mysite/demo/answer/AnswerService.java
@@ -0,0 +1,23 @@
+package com.mysite.demo.answer;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.mysite.demo.question.Question;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+	private final AnswerRepository answerRepository;
+	
+	public void create(Question question, String content) {
+		Answer answer = new Answer();
+		answer.setContent(content);
+		answer.setCreateDate(LocalDateTime.now());
+		answer.setQuestion(question);
+		this.answerRepository.save(answer);
+	}
+}
