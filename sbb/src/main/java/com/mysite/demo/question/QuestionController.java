d7a93f992a1202ddc0889ad28c022c5a8bbbf7aa
황승아 <mina789@naver.com>
2024-04-15 06:55:11 +0900

    답변등록 vaildator

--- sbb/src/main/java/com/mysite/demo/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/demo/question/QuestionController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mysite.demo.answer.AnswerForm;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,7 +34,7 @@ public class QuestionController {
 	}
 	
 	@GetMapping(value = "/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
