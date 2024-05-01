8433a9af4374cd778676dd1c8a9ad7138ff2a7a2
황승아 <mina789@naver.com>
2024-05-01 23:32:06 +0900

    에러 출력

--- sbb/src/main/java/com/mysite/demo/user/UserController.java
+++ sbb/src/main/java/com/mysite/demo/user/UserController.java
@@ -1,5 +1,6 @@
 package com.mysite.demo.user;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,8 +31,18 @@ public class UserController {
 			bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
 			return "signup_form";
 		}
+		try {
+			userService.create(userCrateForm.getUsername(), userCrateForm.getEmail(), userCrateForm.getPassword1());
+		}catch(DataIntegrityViolationException e) {
+			e.printStackTrace();
+			bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+			return "signup_form";
+		}catch(Exception e) {
+			e.printStackTrace();
+			bindingResult.reject("signupFailed", e.getMessage());
+			return "signup_form";
+		}
 		
-		userService.create(userCrateForm.getUsername(), userCrateForm.getEmail(), userCrateForm.getPassword1());
 		return "redirect:/";
 	}
 }
